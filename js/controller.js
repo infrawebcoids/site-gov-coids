f21a92862d05f634fcaeffba467f8e6d7ebc4bd1
Caiohh <caiohenrik2313@gmail.com>
2023-01-24 15:44:29 -0300

    update-site

---
+++ js/controller.js
@@ -0,0 +1,5 @@
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+  return new bootstrap.Dropdown(dropdownToggleEl)
+})
+
