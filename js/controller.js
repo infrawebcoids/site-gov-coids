36ff5cc4265dd6b80843b0c2f73151f7ba91c776
infrawebcoids <coids.infraweb@inpe.br>
2023-01-26 09:42:59 -0300

    site-refeito

--- js/controller.js
+++ js/controller.js
@@ -3,3 +3,12 @@ var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
   return new bootstrap.Dropdown(dropdownToggleEl)
 })
 
+const botao = document.querySelector(".btn-fixed");
+
+window.addEventListener("scroll", function (event) {
+  if (window.scrollY == 0) {
+    botao.classList.remove("visible");
+  } else {
+    botao.classList.add("visible")
+  }
+});
